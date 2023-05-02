a26156fdd3825096b7ed87e3afc1804e8d6dccee
Khaledheerae <khaledheera@gmail.comm>
2023-05-02 15:05:25 +0600

    data collected for chef card

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const cors = require('cors');
+const chefs = require('./Data/chef.json')
 
 app.use(cors());
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
+});
+
+app.get('/chefs', (req, res) => {
+	res.send(chefs);
 });
 
 
